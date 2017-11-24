64c09728c60b42341fa188609972da03f617915f
CSharpFri <CSharpFri@CLASS2-05>
2017-11-24 19:01:31 +0100

    Add project files.

---
+++ Laborator/Mikroskop.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator
+{
+    class Mikroskop : Pristroj, Ovladanie
+    {
+
+        public override void VypisDetailniInfo()
+        {
+            base.VypisInfo();
+            Console.WriteLine("Jsme mikroskop");
+            base.VypisStav();
+        }
+        public void Zapni()
+        {
+            zapnuto = true;
+        }
+        public void Vypni()
+        {
+            zapnuto = false;
+        }
+    }
+}
---
+++ Laborator/odstredivka.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laborator
+{
+   
+    class odstredivka : Pristroj, Ovladanie
+    {
+
+        public override void VypisDetailniInfo()
+        {
+            base.VypisInfo();
+            Console.WriteLine("Jsme odstredivka");
+            base.VypisStav();
+        }
+        public void Zapni()
+        {
+            zapnuto = true;
+        }
+        public void Vypni()
+        {
+            zapnuto = false;
+        }
+    
+    }
+}
